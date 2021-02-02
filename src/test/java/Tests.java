13e2ce71c73bd376c4e58bb80051740d5c701b44
Vasiliy Golban <vgolban@accesssoftek.com>
2021-02-02 14:01:28 +0200

    Add JUnit with empty test

---
+++ src/test/java/Tests.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Tests {
+
+    @Test
+    void EmptyTest(){
+        assertTrue(true);
+    }
+}
